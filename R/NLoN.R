6a747c7134b038e3d16715e9f8d52a7f3a5a534c
Claes Maëlick <himself@maelick.net>
2018-03-08 20:35:10 +0200

    Package main man page example fix

--- R/NLoN.R
+++ R/NLoN.R
@@ -10,16 +10,15 @@
 #' data(nlon.data)
 #'
 #' ## Build a model with glmnet
-#' model <- with(nlon.data, NLoNModel(text, rater2, TriGramsAndFeatures))
+#' model <- with(nlon.data, NLoNModel(text, rater2))
 #'
 #' ## Use the model to preidct new data.
 #' topredict <- c("This is natural language.", "not(natural, language);")
-#' NLoNPredict(model, topredict, 0.1, features=FeatureExtraction)
+#' NLoNPredict(model, topredict, 0.1)
 #'
 #' ## Train and predict in a single function call.
 #' NLoN(rbind(nlon.data[, list(text, response=rater2)],
-#'           list(text=topredict), fill=TRUE),
-#'     0.1, features=FeatureExtraction)
+#'           list(text=topredict), fill=TRUE), 0.1)
 #'
 #' @docType package
 #' @name NLoN
